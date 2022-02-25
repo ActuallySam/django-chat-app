487541aab66ddf77a5e0a03c9e07a467ed8b2cd7
Easebuzz Pvt Ltd <samarth.srivastava>
2022-02-25 14:42:50 +0530

    Added Channel layer

--- justchat/settings.py
+++ justchat/settings.py
@@ -72,6 +72,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'justchat.wsgi.application'
 ASGI_APPLICATION = 'justchat.asgi.application'
 
+# CHANNELS
+ASGI_APPLICATION = 'justchat.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
