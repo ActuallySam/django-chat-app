97d16ddd9c5ee95b6d085294a8f0c6be4e3a6600
Easebuzz Pvt Ltd <samarth.srivastava>
2022-02-28 18:04:18 +0530

    Updated UI, UX, Login/SignUp Auth

--- chat/models.py
+++ chat/models.py
@@ -11,7 +11,7 @@ class Message(models.Model):
     def __str__(self):
         return self.author.username
     
-    def last_10_messages(self):
+    def last_10_messages():
         return Message.objects.order_by('-timestamp').all()[:10]
 
     
